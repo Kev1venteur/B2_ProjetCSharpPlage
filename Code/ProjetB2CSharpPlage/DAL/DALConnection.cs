8be34f53f25dbb53a05afdb7bcb89cca81cff7c6
Kev1venteur <kvin.gillet@gmail.com>
2019-11-26 14:47:50 +0100

    init projet et update base

---
+++ Code/ProjetB2CSharpPlage/DAL/DALConnection.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+
+namespace ProjetB2CSharpPlage.ORM
+{
+    class DALConnection
+    {
+        private static string server;
+        private static string database;
+        private static string uid;
+        private static string password;
+        public static MySqlConnection connection;
+
+        public static Boolean OpenConnection()
+        {
+            if (connection == null) //  si la connexion est déjà ouverte, il ne la refera pas 
+            {
+
+                server = "localhost";
+                database = "projetb2csharp";
+                uid = "projetb2csharp";
+                password = "projetb2csharp";
+                string connectionString;
+                connectionString = "SERVER=" + server + ";" + "DATABASE=" +
+                database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+                connection = new MySqlConnection(connectionString);
+                connection.Open();
+            }
+            return true;
+        }
+    }
+}
